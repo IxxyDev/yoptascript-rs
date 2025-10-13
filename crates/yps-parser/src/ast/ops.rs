14f3dea601818daacb5ccf676ce9dbd6c388d058
IxxyDev <denisenkoforwork@gmail.com>
2025-10-13 18:19:51 +0300

    feat: add operators scaffold

---
+++ crates/yps-parser/src/ast/ops.rs
@@ -0,0 +1,16 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum UnaryOp {
+  Plus,
+  Minus,
+  // TODO: Add other operators
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum BinaryOp {
+  Add,
+  Sub,
+  Assign,
+  Equals,
+  StrictEquals
+  // TODO: Add other operators
+}
