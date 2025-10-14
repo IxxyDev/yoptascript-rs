fb5d78633d816be0fc6447d06245031f5c03013a
IxxyDev <denisenkoforwork@gmail.com>
2025-10-14 16:57:03 +0300

    chore: remove tracked target artifacts

--- target/debug/deps/yopta_rs-256478621c35c2d3.d
+++
@@ -1,5 +0,0 @@
-/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/target/debug/deps/yopta_rs-256478621c35c2d3.d: src/main.rs
-
-/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/target/debug/deps/libyopta_rs-256478621c35c2d3.rmeta: src/main.rs
-
-src/main.rs:
