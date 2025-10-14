f815f9573afeecc3e66595c66825774cbe4075d4
IxxyDev <denisenkoforwork@gmail.com>
2025-10-14 16:50:08 +0300

    chore: fix clippy invocation in hook

---
+++ target/debug/deps/yps_parser-3d0837ef4682345e.d
@@ -0,0 +1,18 @@
+/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/target/debug/deps/yps_parser-3d0837ef4682345e.d: /Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/clippy.toml crates/yps-parser/src/lib.rs crates/yps-parser/src/ast/mod.rs crates/yps-parser/src/ast/expr.rs crates/yps-parser/src/ast/ident.rs crates/yps-parser/src/ast/literal.rs crates/yps-parser/src/ast/ops.rs crates/yps-parser/src/ast/program.rs crates/yps-parser/src/ast/stmt.rs crates/yps-parser/src/parser/mod.rs Cargo.toml
+
+/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/target/debug/deps/libyps_parser-3d0837ef4682345e.rmeta: /Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/clippy.toml crates/yps-parser/src/lib.rs crates/yps-parser/src/ast/mod.rs crates/yps-parser/src/ast/expr.rs crates/yps-parser/src/ast/ident.rs crates/yps-parser/src/ast/literal.rs crates/yps-parser/src/ast/ops.rs crates/yps-parser/src/ast/program.rs crates/yps-parser/src/ast/stmt.rs crates/yps-parser/src/parser/mod.rs Cargo.toml
+
+/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/clippy.toml:
+crates/yps-parser/src/lib.rs:
+crates/yps-parser/src/ast/mod.rs:
+crates/yps-parser/src/ast/expr.rs:
+crates/yps-parser/src/ast/ident.rs:
+crates/yps-parser/src/ast/literal.rs:
+crates/yps-parser/src/ast/ops.rs:
+crates/yps-parser/src/ast/program.rs:
+crates/yps-parser/src/ast/stmt.rs:
+crates/yps-parser/src/parser/mod.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-W__CLIPPY_HACKERY__clippy::pedantic__CLIPPY_HACKERY__-W__CLIPPY_HACKERY__clippy::nursery__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
