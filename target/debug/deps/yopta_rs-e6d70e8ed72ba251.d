75382a4e5bdbfe6f10cf229ff07c74e4e961e6af
IxxyDev <denisenkoforwork@gmail.com>
2025-10-16 22:50:03 +0300

    upd json

---
+++ target/debug/deps/yopta_rs-e6d70e8ed72ba251.d
@@ -0,0 +1,10 @@
+/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/target/debug/deps/yopta_rs-e6d70e8ed72ba251.d: /Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/clippy.toml src/main.rs Cargo.toml
+
+/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/target/debug/deps/libyopta_rs-e6d70e8ed72ba251.rmeta: /Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/clippy.toml src/main.rs Cargo.toml
+
+/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/clippy.toml:
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-W__CLIPPY_HACKERY__clippy::pedantic__CLIPPY_HACKERY__-W__CLIPPY_HACKERY__clippy::nursery__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ target/debug/deps/yps_lexer-22439bb4ee71bdd0.d
@@ -0,0 +1,5 @@
+/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/target/debug/deps/yps_lexer-22439bb4ee71bdd0.d: crates/yps-lexer/src/lib.rs
+
+/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/target/debug/deps/libyps_lexer-22439bb4ee71bdd0.rmeta: crates/yps-lexer/src/lib.rs
+
+crates/yps-lexer/src/lib.rs:
