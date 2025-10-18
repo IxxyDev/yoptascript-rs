f137e9c0a9d61de570ad6e80a587bd628112edfd
IxxyDev <denisenkoforwork@gmail.com>
2025-10-18 23:42:30 +0300

    refactor: remove target

--- target/debug/deps/yopta_rs-0cf1de35c5970bbf.d
+++
@@ -1,5 +0,0 @@
-/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/target/debug/deps/yopta_rs-0cf1de35c5970bbf.d: src/main.rs
-
-/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/target/debug/deps/libyopta_rs-0cf1de35c5970bbf.rmeta: src/main.rs
-
-src/main.rs:
