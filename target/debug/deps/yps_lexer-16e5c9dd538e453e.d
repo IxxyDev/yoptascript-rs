d8c54c3d6d995c5487facc04aa1d8f4418012d89
IxxyDev <denisenkoforwork@gmail.com>
2025-10-18 23:42:30 +0300

    refactor: remove target

--- target/debug/deps/yps_lexer-16e5c9dd538e453e.d
+++
@@ -1,5 +0,0 @@
-/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/target/debug/deps/yps_lexer-16e5c9dd538e453e.d: crates/yps-lexer/src/lib.rs
-
-/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/target/debug/deps/libyps_lexer-16e5c9dd538e453e.rmeta: crates/yps-lexer/src/lib.rs
-
-crates/yps-lexer/src/lib.rs:
--- target/debug/deps/yps_lexer-ca40c6d1aae2af65.d
+++
@@ -1,5 +0,0 @@
-/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/target/debug/deps/yps_lexer-ca40c6d1aae2af65.d: crates/yps-lexer/src/lib.rs
-
-/Users/mcstudio_yaroslav/dev/rust/yoptascript-rs/target/debug/deps/yps_lexer-ca40c6d1aae2af65: crates/yps-lexer/src/lib.rs
-
-crates/yps-lexer/src/lib.rs:
