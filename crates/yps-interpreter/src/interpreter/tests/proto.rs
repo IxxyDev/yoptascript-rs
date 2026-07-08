94b815b0cd230af34d49bf28381e8950a52db599
IxxyDev <denisenkoforwork@gmail.com>
2026-07-09 02:28:31 +0300

    feat(stdlib): property descriptor API and accessor-aware имеетСвоё on Кент

--- crates/yps-interpreter/src/interpreter/tests/proto.rs
+++ crates/yps-interpreter/src/interpreter/tests/proto.rs
@@ -228,3 +228,246 @@ fn proto_keys_does_not_expose_internals() {
     );
     assert_eq!(interp.get("дл"), Some(Value::Number(1.0)));
 }
+
+#[test]
+fn define_property_data_form_readable() {
+    let interp = run_code(
+        r#"
+        гыы o = {};
+        Кент.определитьСвойство(o, "x", { значение: 42 });
+        гыы v = o.x;
+        "#,
+    );
+    assert_eq!(interp.get("v"), Some(Value::Number(42.0)));
+}
+
+#[test]
+fn define_property_accessor_getter_invoked() {
+    let interp = run_code(
+        r#"
+        гыы cnt = 0;
+        гыы o = {};
+        Кент.определитьСвойство(o, "y", {
+            получить: () => { cnt = cnt + 1; }
+        });
+        гыы a = o.y;
+        гыы b = o.y;
+        "#,
+    );
+    assert_eq!(interp.get("cnt"), Some(Value::Number(2.0)));
+}
+
+#[test]
+fn define_property_accessor_setter_invoked() {
+    let interp = run_code(
+        r#"
+        гыы captured = 0;
+        гыы o = {};
+        Кент.определитьСвойство(o, "z", {
+            установить: (v) => { captured = v; }
+        });
+        o.z = 99;
+        "#,
+    );
+    assert_eq!(interp.get("captured"), Some(Value::Number(99.0)));
+}
+
+#[test]
+fn define_property_accessor_replaces_data_property() {
+    let interp = run_code(
+        r#"
+        гыы o = { p: 1 };
+        Кент.определитьСвойство(o, "p", { получить: () => 2 });
+        гыы v = o.p;
+        гыы has_data = Кент.имеетСвоё(o, "p");
+        "#,
+    );
+    assert_eq!(interp.get("v"), Some(Value::Number(2.0)));
+    assert_eq!(interp.get("has_data"), Some(Value::Boolean(true)));
+}
+
+#[test]
+fn has_own_true_for_getter_only_accessor() {
+    let interp = run_code(
+        r#"
+        гыы o = {};
+        Кент.определитьСвойство(o, "p", { получить: () => 2 });
+        гыы has = Кент.имеетСвоё(o, "p");
+        "#,
+    );
+    assert_eq!(interp.get("has"), Some(Value::Boolean(true)));
+}
+
+#[test]
+fn has_own_true_for_setter_only_accessor() {
+    let interp = run_code(
+        r#"
+        гыы o = {};
+        Кент.определитьСвойство(o, "p", { установить: (v) => {} });
+        гыы has = Кент.имеетСвоё(o, "p");
+        "#,
+    );
+    assert_eq!(interp.get("has"), Some(Value::Boolean(true)));
+}
+
+#[test]
+fn has_own_true_for_getter_and_setter_accessor() {
+    let interp = run_code(
+        r#"
+        гыы o = {};
+        Кент.определитьСвойство(o, "p", { получить: () => 2, установить: (v) => {} });
+        гыы has = Кент.имеетСвоё(o, "p");
+        "#,
+    );
+    assert_eq!(interp.get("has"), Some(Value::Boolean(true)));
+}
+
+#[test]
+fn has_own_true_for_plain_property() {
+    let interp = run_code(
+        r#"
+        гыы o = { p: 1 };
+        гыы has = Кент.имеетСвоё(o, "p");
+        "#,
+    );
+    assert_eq!(interp.get("has"), Some(Value::Boolean(true)));
+}
+
+#[test]
+fn has_own_false_for_missing_property() {
+    let interp = run_code(
+        r#"
+        гыы o = { p: 1 };
+        гыы has = Кент.имеетСвоё(o, "отсутствует");
+        "#,
+    );
+    assert_eq!(interp.get("has"), Some(Value::Boolean(false)));
+}
+
+#[test]
+fn has_own_false_for_internal_getter_key_query() {
+    let interp = run_code(
+        r#"
+        гыы o = {};
+        Кент.определитьСвойство(o, "p", { получить: () => 2 });
+        гыы has = Кент.имеетСвоё(o, "__get_p__");
+        "#,
+    );
+    assert_eq!(interp.get("has"), Some(Value::Boolean(false)));
+}
+
+#[test]
+fn keys_still_omit_accessor_only_properties() {
+    let interp = run_code(
+        r#"
+        гыы o = { a: 1 };
+        Кент.определитьСвойство(o, "p", { получить: () => 2 });
+        гыы ks = Кент.ключи(o);
+        гыы n = длина(ks);
+        "#,
+    );
+    assert_eq!(interp.get("n"), Some(Value::Number(1.0)));
+}
+
+#[test]
+fn define_property_data_replaces_accessor_property() {
+    let interp = run_code(
+        r#"
+        гыы o = {};
+        Кент.определитьСвойство(o, "p", { получить: () => 2 });
+        Кент.определитьСвойство(o, "p", { значение: 7 });
+        гыы v = o.p;
+        "#,
+    );
+    assert_eq!(interp.get("v"), Some(Value::Number(7.0)));
+}
+
+#[test]
+fn define_property_value_and_accessor_conflict_errors() {
+    let err = run_code_err(
+        r#"
+        гыы o = {};
+        Кент.определитьСвойство(o, "p", { значение: 1, получить: () => 2 });
+        "#,
+    );
+    assert!(err.message.contains("значение") && err.message.contains("получить"));
+}
+
+#[test]
+fn define_property_no_descriptor_fields_is_undefined() {
+    let interp = run_code(
+        r#"
+        гыы o = {};
+        Кент.определитьСвойство(o, "p", {});
+        гыы v = o.p;
+        "#,
+    );
+    assert_eq!(interp.get("v"), Some(Value::Undefined));
+}
+
+#[test]
+fn get_own_property_descriptor_data_form() {
+    let interp = run_code(
+        r#"
+        гыы o = { p: 5 };
+        гыы d = Кент.описатьСвойство(o, "p");
+        гыы v = d.значение;
+        "#,
+    );
+    assert_eq!(interp.get("v"), Some(Value::Number(5.0)));
+}
+
+#[test]
+fn get_own_property_descriptor_accessor_form() {
+    let interp = run_code(
+        r#"
+        гыы o = {};
+        Кент.определитьСвойство(o, "p", { получить: () => 1, установить: (v) => {} });
+        гыы d = Кент.описатьСвойство(o, "p");
+        гыы g = d.получить;
+        гыы s = d.установить;
+        "#,
+    );
+    let g = interp.get("g").unwrap();
+    let s = interp.get("s").unwrap();
+    assert!(g.is_callable());
+    assert!(s.is_callable());
+}
+
+#[test]
+fn get_own_property_descriptor_missing_is_undefined() {
+    let interp = run_code(
+        r#"
+        гыы o = {};
+        гыы d = Кент.описатьСвойство(o, "нет");
+        "#,
+    );
+    assert_eq!(interp.get("d"), Some(Value::Undefined));
+}
+
+#[test]
+fn get_own_property_descriptor_hides_internal_keys() {
+    let interp = run_code(
+        r#"
+        клёво К {
+            поле = 5;
+        }
+        гыы x = захуярить К();
+        гыы d = Кент.описатьСвойство(x, "__class__");
+        "#,
+    );
+    assert_eq!(interp.get("d"), Some(Value::Undefined));
+}
+
+#[test]
+fn define_property_on_frozen_object_is_noop() {
+    let interp = run_code(
+        r#"
+        гыы o = { p: 1 };
+        Кент.заморозить(o);
+        Кент.определитьСвойство(o, "p", { значение: 2 });
+        гыы v = o.p;
+        "#,
+    );
+    assert_eq!(interp.get("v"), Some(Value::Number(1.0)));
+}
